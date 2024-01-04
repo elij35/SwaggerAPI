d39c589d0937127f93cda38cdf4345bfc69fb40c
elij35 <elijbowen@gmail.com>
2024-01-04 19:02:49 +0000

    Updated swagger options

--- webapp/Program.cs
+++ webapp/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
-using webapp.Model;
+using Microsoft.OpenApi.Models;
+using webapp;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,8 +9,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("DBCS")));
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Swagger Profile API",
+        Version = "v1"
+    });
+});
 
 var app = builder.Build();
 
@@ -17,7 +26,7 @@ app.UseSwagger();
 
 app.UseSwaggerUI(options =>
 {
-    options.SwaggerEndpoint("/COMP2001/ebowen/swagger/v1/swagger.json", "v1");
+    options.SwaggerEndpoint("/COMP2001/EBowen/swagger/v1/swagger.json", "v1");
 });
 
 app.UseHttpsRedirection();
