e5e9894a58d33201ec60694ff1082496de029eba
elij35 <elijbowen@gmail.com>
2024-01-04 18:59:39 +0000

    Added controllers

---
+++ webapp/Controllers/AllActivitiesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using webapp.Model;
+
+namespace webapp.Controllers
+{
+    [Route("/activities")]
+    [ApiController]
+    public class AllActivitiesController
+    {
+        private readonly DataContext _dataContext;
+
+        public AllActivitiesController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<ActivityData>> Get()
+        {
+            return await _dataContext.FavouriteActivities.ToListAsync();
+        }
+    }
+}
---
+++ webapp/Controllers/PublicProfileController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using webapp.Model;
+
+namespace webapp.Controllers
+{
+    [Route("user/public/{id}")]
+    [ApiController]
+
+    public class PublicProfileController
+    {
+        private readonly DataContext _dataContext;
+
+        public PublicProfileController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<UserProfile>> Get()
+        {
+            return await _dataContext.UserProfiles.ToListAsync();
+        }
+    }
+}
