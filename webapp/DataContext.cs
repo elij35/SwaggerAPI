a9c10bac535afbd901a614681a3ef5ba6166f869
elij35 <elijbowen@gmail.com>
2024-01-04 19:02:03 +0000

    Created data context file for database

---
+++ webapp/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using webapp.Model;
+
+namespace webapp
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("CW2");
+            modelBuilder.Entity<UserActivityLink>().HasNoKey().ToView("Favourite Activities");
+            modelBuilder.Entity<ActivityName>().HasNoKey().ToView("Activity Names");
+        }
+
+        public DbSet<ActivityData> FavouriteActivities { get; set; }
+        public DbSet<UserActivity> UserProfileFavouriteActivities { get; set; }
+        public DbSet<UserProfile> UserProfiles { get; set; }
+        public DbSet<UserActivityLink> UserActivityLinkView { get; set; }
+        public DbSet<ActivityName> ActivityNamesView { get; set; }
+    }
+}
