d05df572767b43a295d774f856d04a669a484b83
elij35 <elijbowen@gmail.com>
2024-01-05 15:23:52 +0000

    Created an message /error output method

---
+++ webapp/ResultOutput.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json;
+
+namespace webapp
+{
+    public class ResultOutput<T>
+    {
+        public T Output { get; set; }
+
+        public ResultOutput(T _Message)
+        {
+            Output = _Message;
+        }
+
+        public async Task<string> Serialize()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+
+        public static ResultOutput<string> Error = new ResultOutput<string>("Error.");
+    }
+}
