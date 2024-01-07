da58645b5fd629a6598d6ef0cbdb33465e06f5cb
elij35 <elijbowen@gmail.com>
2024-01-07 00:04:36 +0000

    Made options and required fields in tables

--- webapp/Model/UserProfile.cs
+++ webapp/Model/UserProfile.cs
@@ -9,30 +9,36 @@ namespace webapp.Model
         [Key]
         public int UserID { get; set; }
 
+        [Required]
         public string UserName { get; set; }
 
+        [Required]
         public string Email { get; set; }
 
-        public string Location { get; set; }
+        public string? Location { get; set; }
 
+        [Required]
         public string Password { get; set; }
 
-        public string Birthday { get; set; }
+        public string? Birthday { get; set; }
 
-        public string AboutMe { get; set; }
+        public string? AboutMe { get; set; }
 
-        public string ProfilePicture { get; set; }
+        public string? ProfilePicture { get; set; }
 
+        [Required]
         public string Units { get; set; }
 
+        [Required]
         public string ActivityTimePreference { get; set; }
 
-        public int Height { get; set; }
+        public int? Height { get; set; }
 
-        public int Weight { get; set; }
+        public int? Weight { get; set; }
 
+        [Required]
         public string MarketingLanguage { get; set; }
 
-        public string Admin { get; set; }
+        public int Admin { get; set; }
     }
 }
