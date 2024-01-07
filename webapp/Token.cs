4f6f86b23537c412ca4828a8babf176fd449ac34
elij35 <elijbowen@gmail.com>
2024-01-07 15:41:48 +0000

    Created a random token generator

--- webapp/Token.cs
+++ webapp/Token.cs
@@ -28,5 +28,43 @@ namespace webapp
             securityToken = new Dictionary<string, TokenInfo>();
             getTokenFromID = new Dictionary<int, string>();
         }
+
+        // Authorize a user and generate a new token if necessary
+        public string AuthorizeUser(int userID)
+        {
+            // Check if the user already has a token
+            if (getTokenFromID.TryGetValue(userID, out string existingToken))
+            {
+                return existingToken;
+            }
+
+            // Generate a new token for the user
+            string newToken = GenerateNewToken();
+
+            // Create token information
+            TokenInfo tokenInfo = new TokenInfo
+            {
+                UserID = userID,
+            };
+
+            // Add the new token and token information to the dictionaries
+            securityToken.Add(newToken, tokenInfo);
+            getTokenFromID.Add(userID, newToken);
+
+            // Return the newly generated token
+            return newToken;
+        }
+
+        // Generating a new secure token using RNGCryptoServiceProvider
+        private string GenerateNewToken()
+        {
+            using (var rngCrypto = new RNGCryptoServiceProvider())
+            {
+                byte[] randomBytes = new byte[32];
+                rngCrypto.GetBytes(randomBytes);
+
+                return BitConverter.ToString(randomBytes).Replace("-", "");
+            }
+        }
     }
 }
