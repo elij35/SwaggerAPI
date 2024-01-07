af23f4a485c6a7c2f6f6e0a62787f2ab2a104445
elij35 <elijbowen@gmail.com>
2024-01-07 15:35:45 +0000

    Added new user put request

--- webapp/Controllers/UsersController.cs
+++ webapp/Controllers/UsersController.cs
@@ -24,5 +24,31 @@ namespace webapp.Controllers
                 return await new ResultOutput<PublicUser>(publicUserInfo).Serialize();
             }
         }
+
+        [HttpPut]
+        [Route("/api/user/new")]
+        public async Task<string> CreateNewUser([FromBody] NewUser bodyUser)
+        {
+            int rowsAffected;
+            string sql = "EXEC CW2.CreateNewProfile @Username, @Email, @Password, @Units, @ActivityTimePreference, @MarketingLanguage, @Admin";
+
+            List<SqlParameter> parms = new List<SqlParameter>
+            {
+                // Create parameters    
+                new SqlParameter { ParameterName = "@Username", Value = bodyUser.Username },
+                new SqlParameter { ParameterName = "@Password", Value = bodyUser.Password},
+                new SqlParameter { ParameterName = "@Email", Value = bodyUser.Email},
+                new SqlParameter { ParameterName = "@Units", Value = "Metric"},
+                new SqlParameter { ParameterName = "@ActivityTimePreference", Value = "Speed"},
+                new SqlParameter { ParameterName = "@MarketingLanguage", Value = "English (UK)"},
+                new SqlParameter { ParameterName = "@Admin", Value = 0}
+            };
+
+            using (DataContext db = new DataContext())
+            {
+                rowsAffected = db.Database.ExecuteSqlRaw(sql, parms.ToArray());
+                return await new ResultOutput<string>("User successfully created.").Serialize();
+            }
+        }
     }
 }
