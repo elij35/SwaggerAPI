b12ed08ae396423a789334442fc91dac75933edc
elij35 <elijbowen@gmail.com>
2024-01-07 15:21:10 +0000

    Created Admin controller to delete accounts

---
+++ webapp/Controllers/AdminController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using webapp.Model;
+
+namespace webapp.Controllers
+{
+    [ApiController]
+    public class AdminController
+    {
+
+        [HttpDelete]
+        [Route("/api/admin/delete/{id}")]
+
+        public async Task<string> DeleteUser([FromRoute] int id, [FromBody] AuthenticationAPI.Login adminCredentials)
+        {
+            using (DataContext dbContext = new DataContext())
+            {
+                // Check Authentication API to verify admin permission
+                bool isAdmin = await AuthenticationAPI.AuthenticateUser(adminCredentials.Email, adminCredentials.Password);
+
+                if (!isAdmin)
+                {
+                    return await new ResultOutput<string>("Unauthorized: Not an admin").Serialize();
+                }
+
+                // Check database for the user to be deleted
+                UserProfile userToDelete = dbContext.UserProfiles.FirstOrDefault(user => user.UserID == id);
+
+                if (userToDelete == null)
+                {
+                    return await new ResultOutput<string>("User not found in the database").Serialize();
+                }
+
+                dbContext.UserProfiles.Remove(userToDelete);
+                await dbContext.SaveChangesAsync();
+
+                return await new ResultOutput<string>("User deleted successfully").Serialize();
+            }
+        }
+    }
+}
