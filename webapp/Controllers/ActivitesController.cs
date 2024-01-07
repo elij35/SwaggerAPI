da2eba3074be5e6cb46c0b7a6a371ded12975faf
elij35 <elijbowen@gmail.com>
2024-01-07 00:05:09 +0000

    Created an activities controller

---
+++ webapp/Controllers/ActivitesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using webapp.Model;
+
+namespace webapp.Controllers
+{
+    [ApiController]
+    public class ActivitiesController
+    {
+        [HttpGet]
+        [Route("/api/user/activities")]
+        public async Task<IEnumerable<ActivityData>> Get()
+        {
+            return await new DataContext().FavouriteActivities.ToListAsync();
+        }
+
+    }
+}
