552c0033a4ef8ac4ec87f3470658308550d27aa8
elij35 <elijbowen@gmail.com>
2024-01-07 15:34:57 +0000

    Added public account view

---
+++ webapp/Controllers/UsersController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using webapp.Model;
+
+namespace webapp.Controllers
+{
+    [ApiController]
+    public class UsersController
+    {
+        [HttpGet]
+        [Route("/api/user/public/{id}")]
+        public async Task<string> GetPublicUserProfile([FromRoute] int id)
+        {
+            using (DataContext dbContext = new DataContext())
+            {
+                UserProfile userProfile = dbContext.UserProfiles.FirstOrDefault(user => user.UserID == id);
+
+                if (userProfile == null)
+                {
+                    return await new ResultOutput<string>("User profile not found").Serialize();
+                }
+                PublicUser publicUserInfo = new PublicUser(userProfile.UserName, userProfile.AboutMe, userProfile.ProfilePicture);
+                return await new ResultOutput<PublicUser>(publicUserInfo).Serialize();
+            }
+        }
+    }
+}
